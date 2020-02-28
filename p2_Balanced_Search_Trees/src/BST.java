2ffdff16d907eaaa51396ca8b0ba164f372b3ccc
korYuSa <rtgh64@gmail.com>
2020-02-28 15:31:06 -0600

    Update BST.java

--- p2_Balanced_Search_Trees/src/BST.java
+++ p2_Balanced_Search_Trees/src/BST.java
@@ -16,17 +16,39 @@ import java.util.List;
  */
 public class BST<K extends Comparable<K>, V> implements STADT<K, V> {
 
+  /**
+   * Node sub-class of the BST
+   * 
+   * @author Jun Jeong
+   *
+   * @param <K> A Comparable type to be used as a key to an associated value.
+   * @param <V> A value associated with the given key.
+   */
+  @SuppressWarnings("hiding")
   private class Node<K, V> {
-    private K key;
-    private V value;
-    private Node<K, V> left;
-    private Node<K, V> right;
+    private K key; //Key of the node
+    private V value; //Value of the node
+    private Node<K, V> left; //Left subtree/node of the node
+    private Node<K, V> right; //Right subtree/node of the node
 
+    /**
+     * Constructor of the node
+     * 
+     * @param k A key to an associated value.
+     * @param v A value associated with the given key.
+     */
     public Node(K k, V v) {
       this.key = k;
       this.value = v;
     }
-
+    
+    /**
+     * Returns true if key and value are equal to each other. Otherwise, returns false.
+     * 
+     * @param o An object to compare with the node
+     * @return true if key and value are equal to each other
+     */
+    @SuppressWarnings("rawtypes")
     public boolean equal(Object o) {
       if (o instanceof BST.Node) {
         if (((BST.Node) o).key.equals(this.key) && ((BST.Node) o).value.equals(this.value))
@@ -38,8 +60,8 @@ public class BST<K extends Comparable<K>, V> implements STADT<K, V> {
   }
 
 
-  private Node<K, V> root;
-  private int size = 0;
+  private Node<K, V> root; //root node of the BST
+  private int size = 0; //size a.k.a. number of nodes in the BST
 
 
   /**
@@ -48,20 +70,35 @@ public class BST<K extends Comparable<K>, V> implements STADT<K, V> {
    * @return key found at root node, or null
    */
   public K getKeyAtRoot() {
+    //if root is null
     if (root == null)
       return null;
+    //otherwise
     return root.key;
   }
 
+  /**
+   * Recursive method to find the node that has specific key.
+   * If node with the key does not exist, returns null.
+   * 
+   * @param root A root node to search
+   * @param key A key of the node to be found
+   * @return node with the key, or null
+   */
   private Node<K, V> getNodeWith(Node<K, V> root, K key) {
+    //if root is null
     if(root == null)
       return null;
+    //else, if root has the key
     if (root.key.equals(key))
       return root;
+    //else, if key of the root is larger than the key
     if (root.key.compareTo(key) > 0)
       return getNodeWith(root.left, key);
+    //otherwise
     if (root.key.compareTo(key) < 0)
       return getNodeWith(root.right, key);
+    //if none of above matches
     return null;
   }
 
@@ -77,13 +114,18 @@ public class BST<K extends Comparable<K>, V> implements STADT<K, V> {
    * @throws KeyNotFoundException if key is not found in this BST
    */
   public K getKeyOfLeftChildOf(K key) throws IllegalNullKeyException, KeyNotFoundException {
+    //if key is null
     if (key == null)
       throw new IllegalNullKeyException();
+    //else, find node with the key
     Node<K, V> keyNode = getNodeWith(root, key);
+    //if the node does not exist
     if (keyNode == null)
       throw new KeyNotFoundException();
+    //else, if the left of the node does not exist
     if (keyNode.left == null)
       return null;
+    //otherwise
     return keyNode.left.key;
   }
 
@@ -99,13 +141,18 @@ public class BST<K extends Comparable<K>, V> implements STADT<K, V> {
    * @throws KeyNotFoundException if key is not found in this BST
    */
   public K getKeyOfRightChildOf(K key) throws IllegalNullKeyException, KeyNotFoundException {
+    //if key is null
     if (key == null)
       throw new IllegalNullKeyException();
+    //else, find node with the key
     Node<K, V> keyNode = getNodeWith(root, key);
+    //if the node does not exist
     if (keyNode == null)
       throw new KeyNotFoundException();
+    //else, if the right of the node does not exist
     if (keyNode.right == null)
       return null;
+    //otherwsie
     return keyNode.right.key;
   }
 
@@ -124,15 +171,24 @@ public class BST<K extends Comparable<K>, V> implements STADT<K, V> {
    * @return the number of levels that contain keys in this BINARY SEARCH TREE
    */
   public int getHeight() {
+    //return the recursive method
     return getHeight(root);
   }
-
+/**
+ * Recursive helper method as well as overloading method of getHeight
+ * 
+ * @param root A root node to find the height
+ * @return height of the node
+ */
   private int getHeight(Node<K, V> root) {
+    //if root is leaf node
     if (root == null)
       return 0;
+    //otherwise
     else {
       int lheight = getHeight(root.left);
       int rheight = getHeight(root.right);
+      //find the larger height from either left or right subtree
       if (lheight > rheight)
         return (lheight + 1);
       else
@@ -154,15 +210,24 @@ public class BST<K extends Comparable<K>, V> implements STADT<K, V> {
     InOrderTraversal(output, root);
     return output;
   }
-
+  
+  /**
+   * Recursive helper method of getInOrderTraversal
+   * Left -> Current -> Right
+   * 
+   * @param list A list to store nodes in order
+   * @param root A root node of the tree
+   */
   private void InOrderTraversal(List<K> list, Node<K, V> root) {
+    //if root is leaf
     if (root == null)
       return;
-    if (root.left != null)
-      InOrderTraversal(list, root.left);
+    //do left subtree
+    InOrderTraversal(list, root.left);
+    //add key of the root to the list
     list.add(root.key);
-    if (root.right != null)
-      InOrderTraversal(list, root.right);
+    //do right subtree
+    InOrderTraversal(list, root.right);
   }
 
   /**
@@ -179,14 +244,23 @@ public class BST<K extends Comparable<K>, V> implements STADT<K, V> {
     return output;
   }
 
+  /**
+   * Recursive helper method of getPreOrderTraversal
+   * Current -> Left -> Right
+   * 
+   * @param list A list to store nodes in order
+   * @param root A root node of the tree
+   */
   private void PreOrderTraversal(List<K> list, Node<K, V> root) {
+    //if root is leaf
     if (root == null)
       return;
+    //add key of the root to the list
     list.add(root.key);
-    if (root.left != null)
-      PreOrderTraversal(list, root.left);
-    if (root.right != null)
-      PreOrderTraversal(list, root.right);
+    //do left subtree
+    PreOrderTraversal(list, root.left);
+    //do right subtree
+    PreOrderTraversal(list, root.right);
   }
 
   /**
@@ -203,13 +277,22 @@ public class BST<K extends Comparable<K>, V> implements STADT<K, V> {
     return output;
   }
 
+  /**
+   * Recursive helper method of getPostOrderTraversal
+   * Left -> Right -> Current
+   * 
+   * @param list A list to store nodes in order
+   * @param root A root node of the tree
+   */
   private void PostOrderTraversal(List<K> list, Node<K, V> root) {
+    //if root is leaf
     if (root == null)
       return;
-    if (root.left != null)
-      PostOrderTraversal(list, root.left);
-    if (root.right != null)
-      PostOrderTraversal(list, root.right);
+    //do left subtree
+    PostOrderTraversal(list, root.left);
+    //do right subtree
+    PostOrderTraversal(list, root.right);
+    //add key of the root to the list
     list.add(root.key);
   }
 
@@ -224,16 +307,27 @@ public class BST<K extends Comparable<K>, V> implements STADT<K, V> {
    */
   public List<K> getLevelOrderTraversal() {
     List<K> output = new ArrayList<K>();
+    //repeat for each level
     for (int i = 1; i <= getHeight(); i++)
       LevelOrderTraversal(root, i, output);
     return output;
   }
 
+  /**
+   * Helper method of getLevelOrderTraversal
+   * Upper Level -> Lower Level
+   * 
+   * @param list A list to store nodes in order
+   * @param root A root node of the tree
+   */
   private void LevelOrderTraversal(Node<K, V> root, int level, List<K> list) {
+    //if root is leaf
     if (root == null)
       return;
+    //if level is 1 (designated level)
     if (level == 1)
       list.add(root.key);
+    //otherwise, search through child nodes
     else if (level > 1) {
       LevelOrderTraversal(root.left, level - 1, list);
       LevelOrderTraversal(root.right, level - 1, list);
@@ -248,12 +342,23 @@ public class BST<K extends Comparable<K>, V> implements STADT<K, V> {
    * not added.
    */
   public void insert(K key, V value) throws IllegalNullKeyException, DuplicateKeyException {
+    //if key is null
     if (key == null)
       throw new IllegalNullKeyException();
+    //othewise
     Node<K, V> input = new Node<K, V>(key, value);
     root = insert(input, root);
   }
 
+  /**
+   * Recursive helper as well as overload method of insert.
+   * Returns the modified root.
+   * 
+   * @param node A node to insert
+   * @param root A root of the tree
+   * @return root with the node inserted or not
+   * @throws DuplicateKeyException if the node already exist
+   */
   private Node<K, V> insert(Node<K, V> node, Node<K, V> root) throws DuplicateKeyException {
     if (root == null) {
       size++;
@@ -276,28 +381,45 @@ public class BST<K extends Comparable<K>, V> implements STADT<K, V> {
    * return false. If key is null, throw IllegalNullKeyException
    */
   public boolean remove(K key) throws IllegalNullKeyException {
+    //if key is null
     if (key == null)
       throw new IllegalNullKeyException();
+    //otherwise
     int sizeBefore = size;
     root = remove(root, key);
+    //if the size does not changed
     if (size == sizeBefore)
       return false;
+    //otherwise
     else
       return true;
   }
 
+  /**
+   * Recursive helper as well as overload method of the remove.
+   * Returns the modified root.
+   * 
+   * @param node A node to insert
+   * @param key A key of the node to remove
+   * @return root with the node removed or not
+   */
   private Node<K, V> remove(Node<K, V> root, K key) {
+    //if root is leaf
     if (root == null)
       return null;
 
+    //if key of the root is lager than the key, go left subtree
     if (root.key.compareTo(key) > 0)
       root.left = remove(root.left, key);
+    //otherwise, go right subtree
     else if (root.key.compareTo(key) < 0)
       root.right = remove(root.right, key);
 
     // if node has one child
+    //if left node is null
     else if (root.left == null)
       return root.right;
+    //if right node is null
     else if (root.right == null)
       return root.left;
     else {
@@ -305,9 +427,10 @@ public class BST<K extends Comparable<K>, V> implements STADT<K, V> {
       Node<K, V> minChild = root.right;
       while (minChild.left != null)
         minChild = minChild.left;
-  
+      //switch node with the in-order successor
       root.key = minChild.key;
       root.value = minChild.value;
+      //remove the successor
       root.right = remove(root.right, minChild.key);
   
       size--;
